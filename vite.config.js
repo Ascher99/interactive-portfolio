ecae5cd72c545c7be64ce277f57cfd18a6200de9
Adam <agrzegorczyk@theeventa.com>
2026-05-29 15:29:03 +0200

    chore: change base path to interactive-portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/interactive-portfolio/',
   root: '.',
   publicDir: 'public',
   build: {
